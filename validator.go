e3cd9787909d1e4d7b6d704b51c9e2557b0d986e
AB0529 <supercrab115@gmail.com>
2020-12-18 15:35:18 -0500

    Fixed validator error

--- validator.go
+++ validator.go
@@ -2,19 +2,15 @@ package prompter
 
 import (
 	"errors"
-	"reflect"
 	"strconv"
 )
 
 // Required does not allow an empty value
 func Required(val interface{}) error {
-	// The reflect value of the result
-	value := reflect.ValueOf(val)
-
-	// If the value passed in is the zero value of the appropriate type
-	if value.IsZero() && value.Kind() != reflect.Bool {
-		return errors.New("Value is required")
+	if val == "" {
+		return errors.New("Value must be required")
 	}
+
 	return nil
 }
 
